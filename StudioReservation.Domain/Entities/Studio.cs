083e5fbdfdefcb5b249d5c531be25aaf17130544
Heitor Ribeiro <heitor_481@hotmail.com>
2019-11-14 21:47:45 -0300

    Creating the StudioApi.Domain

---
+++ StudioReservation.Domain/Entities/Studio.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudioReservation.Domain.Entities
+{
+    public class Studio
+    {
+    }
+}
---
+++ StudioReservation.Domain/ValueObjects/Address.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudioReservation.Domain.ValueObjects
+{
+    public class Address
+    {
+    }
+}
