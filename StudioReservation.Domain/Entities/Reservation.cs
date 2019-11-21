c0d5b6ec7317166ebf3a18683ba7d7d9587b29a7
Heitor Ribeiro <heitor_481@hotmail.com>
2019-11-21 20:59:45 -0300

    Making studio one collection

--- StudioReservation.Domain/Entities/Reservation.cs
+++ StudioReservation.Domain/Entities/Reservation.cs
@@ -7,12 +7,12 @@ namespace StudioReservation.Domain.Entities
 {
     public class Reservation : IIdentity, IValidatable
     {
-        public Reservation(DateTime dateOfTheReservation, Client client, Studio studio)
+        public Reservation(DateTime dateOfTheReservation, Client client)
         {
             this.NumberOfReservation = Guid.NewGuid().ToString().Replace("-", "").Substring(0, 10);
             this.DateOfTheReservation = dateOfTheReservation;
             this.Client = client;
-            this.Studio = studio;
+            this.Studio = new List<Studio>();
             this.StudioRoom = new List<StudioRoom>();
             this.StudioRoomSchedule = new List<StudioRoomSchedule>();
         }
@@ -23,7 +23,7 @@ namespace StudioReservation.Domain.Entities
 
         public virtual Client Client { get; set; }
 
-        public virtual Studio Studio { get; set; }
+        public virtual ICollection<Studio> Studio { get; set; }
 
         public virtual ICollection<StudioRoom> StudioRoom { get; set; }
 
