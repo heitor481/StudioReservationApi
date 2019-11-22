cbe71950aaa584794b28ce23a156d73e72a67e80
Heitor Ribeiro <hribeiro@humana.com>
2019-11-22 16:48:35 -0300

    Creating DBContext and creating mapping

---
+++ StudioReservation.Data/Map/ClientMapping.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StudioReservation.Domain.Entities;
+
+namespace StudioReservation.Data.Map
+{
+    public class ClientMapping : IEntityTypeConfiguration<Client>
+    {
+
+        public void Configure(EntityTypeBuilder<Client> builder)
+        {
+            builder.ToTable("Client");
+            builder.HasKey(t => t.Id);
+            builder.Property(p => p.FirstName).IsFixedLength().IsRequired();
+            builder.Property(p => p.LastName).IsFixedLength().IsRequired();
+            builder.Property(p => p.DateBirth).IsRequired();
+        }
+    }
+}
---
+++ StudioReservation.Data/StudioReservationContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudioReservation.Data
+{
+    public class StudioReservation : DbContext
+    {
+        //on the base part will go the connection string
+        public StudioReservation() : base()
+        {
+        }
+    }
+}
