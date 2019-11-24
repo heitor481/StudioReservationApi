2d4672e3367a989409c622dc783fc1dda7a0cb53
Heitor Ribeiro <heitor_481@hotmail.com>
2019-11-24 13:39:01 -0300

    Making the mapping of client

--- StudioReservation.Data/Map/ClientMapping.cs
+++ StudioReservation.Data/Map/ClientMapping.cs
@@ -6,14 +6,34 @@ namespace StudioReservation.Data.Map
 {
     public class ClientMapping : IEntityTypeConfiguration<Client>
     {
-
         public void Configure(EntityTypeBuilder<Client> builder)
         {
             builder.ToTable("Client");
-            builder.HasKey(t => t.Id);
+            builder.HasKey(p => p.Id);
             builder.Property(p => p.FirstName).IsFixedLength().IsRequired();
             builder.Property(p => p.LastName).IsFixedLength().IsRequired();
             builder.Property(p => p.DateBirth).IsRequired();
+            builder.OwnsOne(p => p.Address, a =>
+            {
+                a.Property(t => t.Country).IsRequired();
+                a.Property(t => t.State).IsRequired();
+                a.Property(t => t.City).IsRequired();
+                a.Property(t => t.Neighborhood).IsRequired();
+                a.Property(t => t.Street).IsRequired();
+                a.Property(t => t.ZipCode).IsRequired();
+            });
+
+            builder.OwnsOne(p => p.Document, a =>
+            {
+                a.Property(t => t.ClientDocument).IsRequired();
+                a.Property(t => t.DocumentType).IsRequired();
+            });
+
+            builder.OwnsOne(p => p.Email, a =>
+            {
+                a.Property(t => t.UserEmail).IsRequired().HasMaxLength(60);
+                a.Property(t => t.Password).IsRequired().HasMaxLength(16);
+            });
         }
     }
 }
