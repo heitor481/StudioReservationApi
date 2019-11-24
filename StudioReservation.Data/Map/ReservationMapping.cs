f7807cc37c86d92bd86da4fd26d1e89ba3637fb8
Heitor Ribeiro <heitor_481@hotmail.com>
2019-11-24 14:14:29 -0300

    Mapping reservation table

---
+++ StudioReservation.Data/Map/ReservationMapping.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StudioReservation.Domain.Entities;
+
+namespace StudioReservation.Data.Map
+{
+    public class ReservationMapping : IEntityTypeConfiguration<Reservation>
+    {
+        public void Configure(EntityTypeBuilder<Reservation> builder)
+        {
+            builder.ToTable("Reservation");
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.NumberOfReservation);
+            builder.Property(p => p.DateOfTheReservation);
+            builder.HasOne(p => p.Client);
+            builder.HasMany(p => p.Studio);
+            builder.HasMany(p => p.StudioRoom);
+            builder.HasMany(p => p.StudioRoomSchedule);
+        }
+    }
+}
---
+++ StudioReservation.Data/Map/StudioRoomScheduleMapping.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StudioReservation.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudioReservation.Data.Map
+{
+    public class StudioRoomScheduleMapping : IEntityTypeConfiguration<StudioRoomSchedule>
+    {
+        public void Configure(EntityTypeBuilder<StudioRoomSchedule> builder)
+        {
+            builder.ToTable("StudioRoomSchedule");
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.StartTime);
+            builder.Property(p => p.EndTime);
+            builder.Property(p => p.Duration);
+            builder.Property(p => p.DayOfWeek);
+            builder.HasOne(p => p.Studio).WithMany(p => p.StudioRoomSchedule);
+            builder.HasOne(p => p.StudioRoom).WithMany(p => p.StudioRoomSchedule);
+        }
+    }
+}
