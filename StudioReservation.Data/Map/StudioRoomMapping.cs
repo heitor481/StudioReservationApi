539619460997a5587d496ad3e7061ee683b6c1a7
Heitor Ribeiro <heitor_481@hotmail.com>
2019-11-24 13:57:00 -0300

    Creating the mapp of StudioRoom

---
+++ StudioReservation.Data/Map/StudioRoomMapping.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StudioReservation.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudioReservation.Data.Map
+{
+    public class StudioRoomMapping : IEntityTypeConfiguration<StudioRoom>
+    {
+        public void Configure(EntityTypeBuilder<StudioRoom> builder)
+        {
+            builder.ToTable("StudioRoom");
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.RoomNumber);
+            builder.Property(p => p.IsReserved);
+            builder.HasOne(p => p.Studio).WithMany(p => p.StudioRoom);
+            builder.HasMany(p => p.StudioRoomSchedule).WithOne(p => p.StudioRoom);
+        }
+    }
+}
