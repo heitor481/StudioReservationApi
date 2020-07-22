77d99b17d08effccad1b573d61a4e42ffc20adb3
Heitor Ribeiro <heitor_481@hotmail.com>
2020-07-22 20:57:29 -0300

    Creating tests and updating packages

---
+++ StudioReservation.Tests/Application/ClientMiddlewareTests.cs
@@ -0,0 +1,61 @@
+﻿using Moq;
+using StudioReservation.Application.Middlewares;
+using StudioReservation.Application.Middlewares.Interfaces;
+using StudioReservation.NewData.Repository.Interfaces;
+using StudioReservation.NewDomain.Entities;
+using StudioReservation.NewDomain.Enum;
+using StudioReservation.NewDomain.ValueObjects;
+using StudioReservation.NewDomain.ViewModel;
+using StudioReservation.Shared.Error;
+using StudioReservation.Shared.Resources;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StudioReservation.Tests.Application
+{
+    public class ClientMiddlewareTests
+    {
+        private readonly IClientMiddleware stub;
+        private readonly Mock<IError> error;
+        private readonly Mock<IClientRepository> clientRepository;
+        private readonly Mock<ISharedResources> sharedResources;
+
+        public ClientMiddlewareTests()
+        {
+            this.clientRepository = new Mock<IClientRepository>();
+            this.sharedResources = new Mock<ISharedResources>();
+            this.error = new Mock<IError>();
+            this.stub = new ClientMiddleware(this.clientRepository.Object, this.error.Object, this.sharedResources.Object);
+        }
+
+        [Fact]
+        public async Task Create_Client_Return_False_If_Firstname_Is_Empty() 
+        {
+            this.clientRepository.Setup(x => x.CreateClient(It.IsAny<Client>()))
+                .Returns(Task.FromResult(false));
+
+            this.error.SetupGet(x => x.Message).Returns(new List<string>
+            {
+                "Username is required"
+            });
+
+            this.sharedResources.SetupGet(x => x.UsernameRequired).Returns("Username is required");
+
+            var createClient = new CreateClientViewModel
+            {
+                FirstName = "",
+                LastName = "Souza",
+                Address = new Address("", "", "", "", "", ""),
+                Email = new Email("", "", ""),
+                Document = new Document("", EDocumentType.CPF),
+                DateOfBirth = new DateTime(1993, 02, 02)
+            };
+
+            bool response = await this.stub.CreateClient(createClient);
+            Assert.False(response);
+        }
+    }
+}
---
+++ StudioReservation.Tests/Application/LoginMiddlewareTests.cs
@@ -0,0 +1,49 @@
+﻿using Moq;
+using StudioReservation.Application.Middlewares;
+using StudioReservation.Application.Middlewares.Interfaces;
+using StudioReservation.NewData.Repository.Interfaces;
+using StudioReservation.Shared.Error;
+using StudioReservation.Shared.Resources;
+using StudioReservation.Shared.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StudioReservation.Tests.Application
+{
+    public class LoginMiddlewareTests
+    {
+        private readonly ILoginMiddleware stub;
+        private readonly Mock<ILoginRepository> loginRepository;
+        private readonly Mock<IEnvironmentVariable> environmentVariable;
+        private readonly Mock<ISharedResources> sharedResources;
+        private readonly Mock<IError> error;
+
+        public LoginMiddlewareTests()
+        {
+            this.loginRepository = new Mock<ILoginRepository>();
+            this.environmentVariable = new Mock<IEnvironmentVariable>();
+            this.sharedResources = new Mock<ISharedResources>();
+            this.error = new Mock<IError>();
+            this.stub = new LoginMiddleware(this.loginRepository.Object, 
+                this.environmentVariable.Object, this.error.Object, this.sharedResources.Object);
+        }
+
+        [Fact]
+        public async Task Authenticate_Returns_Null_When_Username_Is_Empty() 
+        {
+            this.environmentVariable.Setup(x => x.GetEnvironmentVariable(It.IsAny<string>())).Returns("test");
+            this.error.SetupGet(x => x.Message).Returns(new List<string> 
+            {
+                "Username is required"
+            });
+
+            this.sharedResources.SetupGet(x => x.UsernameRequired).Returns("Username is required");
+
+            var response = await this.stub.Authenticate("Test", "Test");
+            Assert.Null(response);
+        }
+    }
+}
